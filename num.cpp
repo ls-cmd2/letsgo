3a9854b951616ffdbf41e6d99bb32ce21739028f
Shashank Shekhar <shekharshashank093@gmail.com>
2023-05-02 13:27:16 +0530

    added

---
+++ num.cpp
@@ -0,0 +1,22 @@
+//Shashank Shekhar
+//title: num.cpp
+//date: 2023-05-02 13:24:12
+//Path: ~/Desktop/Llgit/num.cpp
+//bool cmp (pair<string,int> &a, pair <string ,int> &b){return a.second<b.second;}
+//int mx = *max_element(v.begin(), v.end())
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define fi(n)        for(int i=0;i<n;i++)
+#define el endl;
+signed main() 
+{
+int t;
+cin>>t;
+while(t--)
+{
+    int a=7;
+    int b=9;
+    cout<<a+b;
+}
+}
