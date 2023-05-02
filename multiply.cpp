7aa572dec53a5729694d7afc6909f4a85b534c4d
Shashank Shekhar <shekharshashank093@gmail.com>
2023-05-02 13:44:23 +0530

    added MULTIPLY

---
+++ multiply.cpp
@@ -0,0 +1,17 @@
+//Shashank Shekhar
+//title: multiply.cpp
+//date: 2023-05-02 13:42:14
+//Path: ~/Desktop/Llgit/multiply.cpp
+//bool cmp (pair<string,int> &a, pair <string ,int> &b){return a.second<b.second;}
+//int mx = *max_element(v.begin(), v.end())
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define fi(n)        for(int i=0;i<n;i++)
+#define el endl;
+signed main() 
+{
+int a=5,b=7;
+cout<<a*b;
+cout<<"the prpduct";
+}
