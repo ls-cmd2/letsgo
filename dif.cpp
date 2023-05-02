9689eab07fec3b0c4afecf3d33c34dd3cf27b8e5
Shashank Shekhar <shekharshashank093@gmail.com>
2023-05-02 14:03:52 +0530

    added dif

---
+++ dif.cpp
@@ -0,0 +1,16 @@
+//Shashank Shekhar
+//title: dif.cpp
+//date: 2023-05-02 14:02:30
+//Path: ~/Desktop/Llgit/dif.cpp
+//bool cmp (pair<string,int> &a, pair <string ,int> &b){return a.second<b.second;}
+//int mx = *max_element(v.begin(), v.end())
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define fi(n)        for(int i=0;i<n;i++)
+#define el endl;
+signed main() 
+{
+int a=9,b=0;
+cout<<a-b;
+}
